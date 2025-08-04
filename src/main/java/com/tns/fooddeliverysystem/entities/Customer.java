9a45dd0b8cb6ce849112877d31555baff2e36d35
Shreya <shreyapawar811@gmail.com>
2025-08-04 23:22:09 +0530

    final commit

--- src/main/java/com/tns/fooddeliverysystem/entities/Customer.java
+++ src/main/java/com/tns/fooddeliverysystem/entities/Customer.java
@@ -1,15 +1,13 @@
-package main.java.com.tns.fooddeliverysystem.entities;
+package com.tns.fooddeliverysystem.entities;
 public class Customer extends User{
-    private Cart cart;
-    public Customer(int userId, String userName, long contactNo){
-        super(userId,userName,contactNo);
-        this.cart=new Cart();
+     private Cart cart;
+
+    public Customer(int userId, String username, long contactNo) {
+        super(userId, username, contactNo);
+        this.cart = new Cart();
     }
-    public Cart getCart(){
+
+    public Cart getCart() {
         return cart;
-    }
-    @Override
-    public String tString(){
-        return "Customer{userId="+ getUserId()+",userName='"+getUserName()+"',contactNo="+getContactNo()+"}";
     }
 }
