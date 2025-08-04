d3aa83fd39fa9991d2b5038e5e4676983097f0ca
Sujal Kale <76490094+sujal-kale@users.noreply.github.com>
2025-08-04 11:32:00 +0530

    Update Main.java

--- src/main/java/com/tns/fooddeliverysystem/application/Main.java
+++ src/main/java/com/tns/fooddeliverysystem/application/Main.java
@@ -4,6 +4,7 @@ public class Main {
      public static void main(String[] args) {
         System.out.println("Hello, Food Delivery!");
         System.out.println("Admin");
+        System.out.println("java project");
     }
 
 }
